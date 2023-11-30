7cfb0610ba51a2c298ca2794ce8a82ba104ebfdc
MamtazDev <akternipa941@gmail.com>
2023-11-30 12:53:42 +0600

    initial setup

---
+++ src/Layout/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import girl from "../assets/girl.png";
+
+const AuthLayout = () => {
+  return <div className="auth">
+
+  </div>;
+};
+
+export default AuthLayout;
