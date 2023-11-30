d523032f894b768d230d7b0ddd810abb839a9f4a
MamtazDev <akternipa941@gmail.com>
2023-11-30 18:25:39 +0600

    validate page add

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignIn from "../Components/Auth/SignIn";
 import SignUp from "../Components/Auth/SignUp";
+import ValidEmail from "../Pages/ValidEmail";
+import ValidCell from "../Pages/ValidCell";
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +12,13 @@ export const router = createBrowserRouter([
   {
     path: "/sign-up",
     element: <SignUp/>,
+  },
+  {
+    path: "/validate-email",
+    element: <ValidEmail/>,
+  },
+  {
+    path: "/validate-cell",
+    element: <ValidCell/>,
   },
 ]);
---
+++ src/Utils/Validate.jsx
@@ -0,0 +1,28 @@
+import logo from "../assets/logo-black.png";
+import "../Components/Auth/Auth.css";
+
+const Validate = ({ title, veriation }) => {
+  return (
+    <div className="validate  ">
+      <img src={logo} alt="" />
+      <div className="valid_box text-center">
+        <div style={{ maxWidth: "560px", width: "100%", margin: "auto" }}>
+          <h2>Validate {title}</h2>
+          <p>A unique 6 digit code has been sent</p>
+          <input
+            type="number"
+            className="mb-5"
+            placeholder="Enter 6 digit code sent to your email address"
+          />
+          <div className="d-flex gap-3">
+            <button className="fs-5 p-3 w-100">Validate</button>
+            <button className="fs-5 p-3 w-100">Resend {veriation}</button>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Validate;
