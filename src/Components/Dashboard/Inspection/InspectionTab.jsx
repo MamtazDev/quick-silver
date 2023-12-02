fdabc64633039af3b541d693b66f16330e608fac
MamtazDev <akternipa941@gmail.com>
2023-12-02 17:38:12 +0600

    insure page add

--- src/Components/Dashboard/Inspection/InspectionTab.jsx
+++ src/Components/Dashboard/Inspection/InspectionTab.jsx
@@ -6,6 +6,7 @@ import attachment from "../../../assets/attachments.png";
 import action from "../../../assets/action.png";
 import history from "../../../assets/history.png";
 import "./Inspection.css";
+import Insured from "./Insured";
 
 const InspectionTab = () => {
   const tabs = [
@@ -39,13 +40,22 @@ const InspectionTab = () => {
     },
   ];
   return (
-    <div className="inspection_tab">
-      {tabs.map((data, index) => (
-        <div className="d-flex gap-1 align-items-center tab fs-6 " key={index}>
-          <img src={data.pic} alt="" />
-          <p>{data.title}</p>
-        </div>
-      ))}
+    <div>
+      <div className="inspection_tab">
+        {tabs.map((data, index) => (
+          <div
+            className="d-flex gap-1 align-items-center tab fs-6 "
+            key={index}
+          >
+            <img src={data.pic} alt="" />
+            <p>{data.title}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="inspection_body">
+        <Insured />
+      </div>
     </div>
   );
 };
